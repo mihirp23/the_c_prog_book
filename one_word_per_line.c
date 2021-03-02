d8cbd18d0541a9c3093018021f04047f5ec3e80e
mihirp23 <mihirp38661@gmail.com>
2021-03-02 13:44:45 -0500

    word count example and exercise

---
+++ one_word_per_line.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define IN  1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+/* Exercise 1-12 */
+/* write a program that prints its input one word per line */
+int main() 
+{
+    int c, state;
+    state = OUT;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            state = OUT;
+            // since we are out of a word, let's print
+            // a newline character
+            printf("\n");
+        }
+        else if (state == OUT) {
+            state = IN;
+        }
+
+        // since we are in a line, let's put the character
+        // to output stream
+        if (state == IN) {
+            putchar(c);
+        }
+    }
+
+    return 0;
+}
